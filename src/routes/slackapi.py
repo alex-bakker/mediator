2836d2b4b2ff9344d80d31ec2e37ac6ae9e0b169
Alex Bakker <abakker@digitaled.com>
2019-08-24 14:51:22 -0400

    Skeleton functions

--- src/routes/slackapi.py
+++ src/routes/slackapi.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request
+from models import db, User
+import requests
 
 slackapi = Blueprint('slackapi', __name__)
 
@@ -17,3 +19,14 @@ def postSlackAPI():
         handleChannel()
     elif request_type == "channel_rename":
         pass
+
+
+def handleUser(token):
+    email = getUserEmail(token)
+    user = db.session.query(User).filter(User.user_name==email).first()
+    print(user)
+
+def getUserEmail(token):
+    URL = 'https://slack.com/api/users.identity.email'
+    params = {'token' : token}
+    return requests.get(URL, params=params)
