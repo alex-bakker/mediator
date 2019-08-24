02de5eb0f34b8740cd7c9dc82ff39ad02f62cbd5
Alex Bakker <abakker@digitaled.com>
2019-08-24 12:43:53 -0400

    Revised db

--- src/models.py
+++ src/models.py
@@ -1,7 +1,23 @@
 from main import app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__= 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Text, nullable=False)
+    user_name = db.Column(db.Text, nullable=False)
+
+class Channel(db.Model):
+    __tablename__= 'channel'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    cid = db.Column(db.Text, nullable=False)
+    channel_name = db.Column(db.Text, nullable=False)
 
 class Score(db.Model):
     __tablename__ = 'score'
@@ -9,7 +25,9 @@ class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     daily_average = db.Column(db.Integer, nullable=False)
     date = db.Column(db.Date, nullable=False)
-    name = db.Column(db.Text, nullable=False)
-    channel = db.Column(db.Text, nullable=False)
+    uid = db.Column(db.ForeignKey('user.id'))
+    cid = db.Column(db.ForeignKey('channel.id'))
 
+    user = relationship('User')
+    channel = relationship('Channel')
     
