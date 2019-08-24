ef9d137059824c635aca434286f4562a4c38148e
Adam Wong <adam@adamwong.me>
2019-08-24 14:02:41 -0400

    handle request type

--- src/routes/slackapi.py
+++ src/routes/slackapi.py
@@ -2,8 +2,18 @@ from flask import Blueprint, request
 
 slackapi = Blueprint('slackapi', __name__)
 
+def handleUser():
+    pass
+
+def handleChannel():
+    pass
+
 @slackapi.route('/', methods=['POST'])
 def postSlackAPI():
     data = request.get_json()
-    return data["challenge"]
-    
+    request_type = data["event"]["type"]
+    if request_type == "message":
+        handleUser()
+        handleChannel()
+    elif request_type == "channel_rename":
+        pass
